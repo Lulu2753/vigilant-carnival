44ef96ca394b5d62c411018b240986f45005a0c8
Lulu2753 <muchenpan@icloud.com>
2025-10-23 21:15:51 +0800

    vary

--- textbook/sizeof.c
+++ textbook/sizeof.c
@@ -8,10 +8,12 @@ int main()
 	
 	printf("%zd\n", sizeof('a' + 12));   //单引号转换成int
 
-	printf("%zd\n", sizeof(9.3));
+	printf("%zd\n", sizeof 9.3);  //也可以不用括号
 
 	/*
 	
+	%zd是VS自己提示的输入类型，因为是sizeof输出的
+
 	主要问题是无法做到输入任意字符输出size，目前只能指定输入类型，然后输入数字输出size
 	但这样无意义，指定类型的size都确定了
 	更不能输入字符类型的字母比如char,int等，都会报错
